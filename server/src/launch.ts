505a20710addb872ff4be2dc065a24a14cb12c58
utkarsh <utk.shukla@gmail.com>
2024-09-09 14:56:33 +0530

    updated typescript language server-- not working

--- server/src/launch.ts
+++ server/src/launch.ts
@@ -1,16 +1,6 @@
 import { IWebSocket, WebSocketMessageReader, WebSocketMessageWriter } from 'vscode-ws-jsonrpc'
 import { createConnection, createServerProcess } from 'vscode-ws-jsonrpc/server'
 import { InitializeRequest, Message, InitializeParams } from 'vscode-languageserver'
-import { execSync } from 'child_process';
-
-function getTypescriptServerPath(): string {
-    try {
-        return execSync('which typescript-language-server').toString().trim();
-    } catch (error) {
-        console.error('TypeScript Language Server not found. Please install it globally.');
-        process.exit(1);
-    }
-} 
 
 export const launch = (socket: IWebSocket) => {
     const reader = new WebSocketMessageReader(socket)
@@ -20,12 +10,12 @@ export const launch = (socket: IWebSocket) => {
 
     const serverConnection = createServerProcess(
         'TypeScript',
-        getTypescriptServerPath(),
+        'typescript-language-server',
         ['--stdio']
     )
 
     if (serverConnection) {
-        socketConnection.forward(serverConnection, message => {
+        socketConnection.forward(serverConnection, (message: Message) => {
             if (Message.isRequest(message)) {
                 if (message.method === InitializeRequest.type.method) {
                     const initializeParams = message.params as InitializeParams;
